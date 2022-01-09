0b4e3a65207edec3acb65feadb8aaa0517e98628
chandrakumarreddy <chandrapenugonda655@gmail.com>
2022-01-09 11:22:54 +0530

    add vercel config

---
+++ api/og-image.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+
+router.get("/", async (req, res) => {
+  const browser = await puppeteer.launch(
+    process.env.AWS_EXECUTION_ENV
+      ? {
+          args: chrome.args,
+          executablePath: await chrome.executablePath,
+          headless: chrome.headless,
+        }
+      : {
+          args: [],
+          executablePath:
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        }
+  );
+
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 720,
+    },
+  });
+  const url = req.query.path;
+  const selector = req.query.selector;
+  await page.goto(url);
+  let data;
+  if (selector) {
+    data = await page.locator(selector).screenshot({
+      type: "png",
+    });
+  } else {
+    data = await page.screenshot({
+      type: "png",
+    });
+  }
+  await browser.close();
+  res.setHeader(
+    "Cache-Control",
+    "s-maxage=31536000, max-age=31536000, stale-while-revalidate"
+  );
+  res.setHeader("Content-Type", "image/png");
+  res.end(data);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+const ogImage = require("./api/og-image");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/api/og-image", ogImage);
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
